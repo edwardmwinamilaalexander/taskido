9daba89979a44062782ce930d93a002d0e3dff1d
Edward <twiga24@gmail.com>
2026-02-16 19:14:05 +0000

    added tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_16_130154) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_16_190501) do
+  create_table "tasks", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
